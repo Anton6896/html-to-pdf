73b607552af43e66ab8bec1f10c0837b449aec29
Anton.R <mail.anton.r@gmail.com>
2025-07-25 17:34:42 +0300

    added plan

--- src/soffice_convert.py
+++ src/soffice_convert.py
@@ -23,6 +23,15 @@ async def send(
     logger.info('[%s] got check_file request', request_id, extra={'request_id': request_id})
     try:
         return FileConvertResponse(document='some document data')
+    
+        """
+        1. get file from request 
+        2. save localy as tmp file 
+        3. send file to convertion with 
+        sh -c "soffice --headless --convert-to pdf 1-page.docx --outdir /data && cat /data/myfile.pdf
+        4. read the converted file
+        5. send return it to client
+        """
 
     except Exception as e:  # noqa
         logger.exception('%s unexpected', request_id, extra={'request_id': request_id})
