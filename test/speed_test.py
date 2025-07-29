9bddde800d46ed71b9dc60c2b1f91317347764e1
Anton.R <mail.anton.r@gmail.com>
2025-07-29 20:59:38 +0300

    added test speed

---
+++ test/speed_test.py
@@ -0,0 +1,47 @@
+import asyncio
+import os
+import random
+import time
+
+import httpx
+
+url = 'http://localhost:8022/api/v1/convert_data/'
+ROOT = os.path.dirname(os.path.abspath(__file__))
+data_folder = os.path.join(ROOT, 'data')
+
+
+async def do_test():
+    with open(os.path.join(data_folder, 'file1.txt')) as f:
+        data_1 = {'document': f.read(), 'document_type': 'docx'}
+
+    with open(os.path.join(data_folder, 'file2.txt')) as f:
+        data_2 = {'document': f.read(), 'document_type': 'xlsx'}
+
+    payloads = [data_1, data_2] * 50
+    random.shuffle(payloads)
+    overall_start = time.perf_counter()
+
+    responses = []
+    chunk_size = 10
+    async with httpx.AsyncClient() as client:
+        for i in range(0, len(payloads), chunk_size):
+            print(f'working on {i} : {i + chunk_size}')
+
+            # Start timer for this chunk
+            chunk_start = time.perf_counter()
+            chunk = payloads[i : i + chunk_size]
+            tasks = [client.post(url, json=payload) for payload in chunk]
+            chunk_responses = await asyncio.gather(*tasks)
+            chunk_elapsed = time.perf_counter() - chunk_start
+            print(f'Chunk {i // chunk_size + 1} took {chunk_elapsed:.2f} seconds')
+            responses.extend(chunk_responses)
+
+    # Report overall duration
+    overall_elapsed = time.perf_counter() - overall_start
+    print(f'Total elapsed time: {overall_elapsed:.2f} seconds')
+
+    print(f'results {[(x, y.status_code) for x, y in enumerate(responses, 1)]}')
+
+
+if __name__ == '__main__':
+    asyncio.run(do_test())
