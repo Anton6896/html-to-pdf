ca7f83994a252973edbcfd253f8a8b0683eeb9b9
Anton.R <mail.anton.r@gmail.com>
2025-08-01 10:32:45 +0300

    wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
 import os
 
 import pytest
+from fastapi.testclient import TestClient
+
+from src.main import app
 
 
 @pytest.fixture
 def data_dir():
     dirname = os.path.dirname(__file__)
     return os.path.join(dirname, 'data')
+
+
+@pytest.fixture()
+def client():
+    return TestClient(app)
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,14 +1,25 @@
 import os
 
-import magic
-import requests
+import pytest
+
+pytest_plugins = ('pytest_asyncio',)
+SOFFICE_CONVERT_URL = 'http://localhost:8022/api/convert/'
+
+
+@pytest.mark.asyncio()
+async def test_create_pdf(data_dir, client):
+    with open(os.path.join(data_dir, '1-page.docx'), 'rb') as f:
+        files = {'file': ('1-page.docx', f, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')}
+
+        response = client.post(
+            SOFFICE_CONVERT_URL, files=files, headers={'X-CELLOSIGN-REQUEST-ID': 'test-request-id-001'}
+        )
+
+        assert response.status_code == 200
+        assert response.headers['content-type'] == 'application/pdf'
+
 
 """
-
-"""
-
-SOFFICE_CONVERT_URL = 'http://localhost:8000/api/convert/'
-
 
 def test_xlsx_file(data_dir):
     # pytest -vv /tests/test_service.py::test_xlsx_file
@@ -51,3 +62,6 @@ def test_docx_file_to_pdf(data_dir):
 
     type_of_data = magic.from_buffer(res.content, mime=True)
     assert type_of_data == 'application/pdf'
+
+
+"""
